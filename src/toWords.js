410728c7fbf186b406b592f98cc22109605c9e84
Azamat Soodaev <azamat.soodaev@gmail.com>
2021-12-13 14:30:23 +0600

    v1.10.0: show number parts, tests

--- src/toWords.js
+++ src/toWords.js
@@ -39,10 +39,14 @@ var currencies = {
 
 var defaultOptions = {
     currency: 'KGS',
+	showNumberParts: {
+		integer: true,
+        fractional: true
+	},
     showCurrency: {
         integer: true,
         fractional: true
-    }
+    },
 };
 
 /**
@@ -52,7 +56,7 @@ var defaultOptions = {
  * @returns {string}
  */
 function toWords(number, options) {
-    var words;
+    var words = '';
     var num = parseInt(number, 10);
     var decimalPart = getDecimalPart(number);
     var currentCurrency = defaultOptions.currency;
@@ -67,14 +71,18 @@ function toWords(number, options) {
             'Input is not a safe number, it’s either too large or too small.'
         );
     }
-    words = generateWords(num);
 
     // copy default options
     if (!options || typeof options !== 'object') options = defaultOptions;
     if (!options.hasOwnProperty('currency')) options.currency = defaultOptions.currency;
-    if (!options.hasOwnProperty('showCurrency')) options.showCurrency = defaultOptions.showCurrency;
+
+	if (!options.hasOwnProperty('showCurrency')) options.showCurrency = defaultOptions.showCurrency;
     if (!options.showCurrency.hasOwnProperty('integer')) options.showCurrency.integer = defaultOptions.showCurrency.integer;
     if (!options.showCurrency.hasOwnProperty('fractional')) options.showCurrency.fractional = defaultOptions.showCurrency.fractional;
+
+	if (!options.hasOwnProperty('showNumberParts')) options.showNumberParts = defaultOptions.showNumberParts;
+	if (!options.showNumberParts.hasOwnProperty('integer')) options.showNumberParts.integer = defaultOptions.showNumberParts.integer;
+    if (!options.showNumberParts.hasOwnProperty('fractional')) options.showNumberParts.fractional = defaultOptions.showNumberParts.fractional;
 
     // currency
     if (typeof options.currency === 'string') {
@@ -83,18 +91,25 @@ function toWords(number, options) {
         }
     }
 
-    // show currency
-    if (options.showCurrency.integer === true) {
-        words += ' ' + currencies[currentCurrency].integer;
-    }
+	// integer part
+	if (options.showNumberParts.integer === true) {
+		words = generateWords(num);
 
-	words += ' ' + decimalPart;
+		if (options.showCurrency.integer === true) {
+			words += ' ' + currencies[currentCurrency].integer;
+		}
+	}
 
-    if (options.showCurrency.fractional === true) {
-        words += ' ' + currencies[currentCurrency].fractional;
-    }
+	// fractional part
+	if (options.showNumberParts.fractional === true) {
+		words += ' ' + decimalPart;
 
-    return capitalizeFirstLetter(words);
+		if (options.showCurrency.fractional === true) {
+			words += ' ' + currencies[currentCurrency].fractional;
+		}
+	}
+
+    return capitalizeFirstLetter(words).trim();
 }
 
 function generateWords(number) {
